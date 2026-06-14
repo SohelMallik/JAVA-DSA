f5a7af1b9eddf7bf960af2bd6594a0ec4b8b5441
Sohel Mallik <malliksohel582@gmail.com>
2026-06-14 20:46:03 +0530

    Post Increment

---
+++ Tricky_Question.java/_2Post_IncrementOperator.java
@@ -0,0 +1,11 @@
+public class _2Post_IncrementOperator {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = a++ + ++a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
