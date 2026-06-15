dfcee349717cb44fe60c02fafddb7c835427fc66
Sohel Mallik <malliksohel582@gmail.com>
2026-06-15 09:20:22 +0530

    Add comment

--- _6Array.java/_9Pass_By_reference.java
+++ _6Array.java/_9Pass_By_reference.java
@@ -1,11 +1,11 @@
 public class _9Pass_By_reference {
-    public static void change(int [] arr){
+    public static void change(int [] arr){//
         arr[1]=45;
     }
     public static void main(String [] args){
         int [] x={10,20,340};
         System.out.println("Before change: "+x[1]);
-        change(x);
+        change(x);// Function Call
         System.out.println("After change: "+x[1]);
     }
     
