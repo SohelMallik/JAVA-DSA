e9f2a6e70aa96aaeebabb38dc4ba585d923fd990
Sohel Mallik <malliksohel582@gmail.com>
2026-06-18 09:29:19 +0530

    Searching

---
+++ _6Array.java/_12_searchint.java
@@ -0,0 +1,21 @@
+public class _12_searchint {
+    public static void main(String [] args){
+        int [] arr={10,20,30,40,50};
+        int target=30;
+        boolean flag=false;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==target){
+                flag=true;
+                break;
+            }
+        }
+
+        if(flag){
+            System.out.println("Element found");
+        }else{
+            System.out.println("Element not found");
+        }
+    }
+}
+
