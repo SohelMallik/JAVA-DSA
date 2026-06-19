6c8328a4d7736cc2c55d963868e6b9ad9d4f0d45
Sohel Mallik <malliksohel582@gmail.com>
2026-06-19 10:15:32 +0530

    Commit

--- _6Array.java/_15second_Largest.java
+++ _6Array.java/_15second_Largest.java
@@ -1,19 +1,46 @@
+
 public class _15second_Largest {
+
     public static void main(String[] args) {
-        int [] arr={12,34,45,45,3,4,5,34};
-        int max=Integer.MIN_VALUE;
-        int smax=Integer.MIN_VALUE;
 
-        for(int i=0; i<arr.length-1; i++){
-            if(arr[i]>max) max=arr[i];
-        }
-    
-        for(int i=0; i<arr.length-1; i++){
-            if(arr[i]>smax && arr[i]!= max) smax=arr[i];
+        int[] arr = {12, 34, 45, 45, 3, 4, 5, 34};
+
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > max) {
+                smax = max;
+                max = num;
+            } else if (num > smax && num != max) {
+                smax = num;
+            }
         }
 
-        System.out.println(max);
-        System.out.println(smax);
+        System.out.println("Largest = " + max);
+        System.out.println("Second Largest = " + smax);
+    }
+}
+
+ //ANOTHER APPROACH
+
+// public class _15second_Largest {
+//     public static void main(String[] args) {
+//         int [] arr={12,34,45,45,3,4,5,34};
+//         int max=Integer.MIN_VALUE;
+//         int smax=Integer.MIN_VALUE;
+
+//         for(int i=0; i<arr.length-1; i++){
+//             if(arr[i]>max) max=arr[i];
+//         }
     
-}
-}
+//         for(int i=0; i<arr.length-1; i++){
+//             if(arr[i]>smax && arr[i]!= max) smax=arr[i];
+//         }
+
+//         System.out.println(max);
+//         System.out.println(smax);
+    
+// }
+// }
+
