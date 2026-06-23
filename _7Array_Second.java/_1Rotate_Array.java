ea92bfef3c7e879da091e086e686427ffee542b2
Sohel Mallik <malliksohel582@gmail.com>
2026-06-23 10:59:15 +0530

    Rotate Array

---
+++ _7Array_Second.java/_1Rotate_Array.java
@@ -0,0 +1,21 @@
+// greek For Greek Problem: Rotate Array
+class Solution {
+ 
+    static void rotateArr(int arr[], int d) {
+        int n= arr.length;
+        d=d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+    }
+    static void reverse(int arr[], int i, int j){// Created By User
+        while(i<j){
+            int temp= arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        
+    }
+}
